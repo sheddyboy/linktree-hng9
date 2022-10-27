70b317069563899a4e7f1b3c8dec414610e31b55
Shadrack Itulah <72614183+sheddyboy@users.noreply.github.com>
2022-10-27 18:46:09 +0100

    feat: Add profile section

--- src/layout/ProfileSection.tsx
+++ src/layout/ProfileSection.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import styles from "../styles/profileSection.module.css";
 
 const ProfileSection = () => {
-  return <section>ProfileSection</section>;
+  return (
+    <section className={styles.section}>
+      <div className="container">
+        <div className={styles.wrapper}>
+          <img
+            id="profile__img"
+            className={styles.profileImg}
+            src="/72614183.jpg"
+            alt="profile"
+          />
+          <h1 className={styles.twitter} id="twitter">
+            @E2LA__
+          </h1>
+          <p id="slack">Sheddy</p>
+          <img
+            className={styles.shareButton}
+            src="/_Avatar share button.png"
+            alt="share"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ProfileSection;
