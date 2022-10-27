94267aa811ff9b542c7e3894d20272299fefd3e8
Shadrack Itulah <72614183+sheddyboy@users.noreply.github.com>
2022-10-27 20:57:48 +0100

    feat: Add footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import styles from "../styles/footer.module.css";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className="container">
+        <div className={styles.wrapper}>
+          <img src="/Zuri.Internship_Logo.png" alt="zuri logo" />
+          <p>HNG Internship 9 Frontend Task</p>
+          <img src="/I4G.png" alt="i4g logo" />
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
