f007ad4457a706ffb5e577a77e55ce55840a202c
Shadrack Itulah <72614183+sheddyboy@users.noreply.github.com>
2022-10-27 18:44:17 +0100

    feat: Import global styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+import "../src/styles/global.css";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
