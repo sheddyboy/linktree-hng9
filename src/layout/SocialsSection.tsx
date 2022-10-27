ce867728ef1e0dff1be037140d257da47460d6fd
Shadrack Itulah <72614183+sheddyboy@users.noreply.github.com>
2022-10-27 20:48:07 +0100

    feat: Add Social section

--- src/layout/SocialsSection.tsx
+++ src/layout/SocialsSection.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import styles from "../styles/socialsSection.module.css";
 
 const SocialsSection = () => {
-  return <section>SocialsSection</section>;
+  return (
+    <section>
+      <div className="container">
+        <div className={styles.wrapper}>
+          <a href="https://slack.com/">
+            <img src="/slack.png" alt="slack" />
+          </a>
+          <a href="https://github.com/sheddyboy">
+            <img src="/github.png" alt="github" />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SocialsSection;
