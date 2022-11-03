fd9636fdb00cb2f9df81ee2b0384d4a54e7ce26d
Shadrack Itulah <72614183+sheddyboy@users.noreply.github.com>
2022-11-03 17:12:00 +0100

    feat: Add react-router to index file

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "../src/styles/global.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Contact from "./pages/Contact";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
