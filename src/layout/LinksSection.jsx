7ba0d314ac26d4d0029bdccd614ae121c0a4c89c
Shadrack Itulah <72614183+sheddyboy@users.noreply.github.com>
2022-11-03 17:14:42 +0100

    chore: Add contact link

--- src/layout/LinksSection.jsx
+++ src/layout/LinksSection.jsx
@@ -8,23 +8,43 @@ const LinksSection = () => {
       name: "Twitter Link",
       href: "https://twitter.com/E2LA__",
       id: "twiterLink",
+      externalRoute: true,
     },
-    { name: "Zuri Team", href: "https://training.zuri.team/", id: "btn__zuri" },
-    { name: "Zuri Books", href: "https://books.zuri.team", id: "books" },
+    {
+      name: "Zuri Team",
+      href: "https://training.zuri.team/",
+      id: "btn__zuri",
+      externalRoute: true,
+    },
+    {
+      name: "Zuri Books",
+      href: "https://books.zuri.team",
+      id: "books",
+      externalRoute: true,
+    },
     {
       name: "Python Books",
       href: "https://books.zuri.team/python-for-beginners?ref_id=sheddy",
       id: "book__python",
+      externalRoute: true,
     },
     {
       name: "Background Check for Coders",
       href: "https://background.zuri.team",
       id: "pitch",
+      externalRoute: true,
     },
     {
       name: "Design Books",
       href: "https://books.zuri.team/design-rules",
       id: "book__design",
+      externalRoute: true,
+    },
+    {
+      name: "Contact Me",
+      href: "/contact",
+      id: "contact",
+      externalRoute: false,
     },
   ];
   return (
@@ -33,7 +53,13 @@ const LinksSection = () => {
         <div className={styles.wrapper}>
           {links.map((i) => {
             return (
-              <LinkCard key={i.id} href={i.href} name={i.name} id={i.id} />
+              <LinkCard
+                key={i.id}
+                href={i.href}
+                name={i.name}
+                id={i.id}
+                externalRoute={i.externalRoute}
+              />
             );
           })}
         </div>
