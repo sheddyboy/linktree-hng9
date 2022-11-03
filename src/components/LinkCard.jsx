068158ab97bb2fa4ac5e7fc206a4aebc55cee156
Shadrack Itulah <72614183+sheddyboy@users.noreply.github.com>
2022-11-03 17:13:51 +0100

    feat: dynamically change from Link tag to a tag

--- src/components/LinkCard.jsx
+++ src/components/LinkCard.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import styles from "../styles/linkcard.module.css";
+import { Link } from "react-router-dom";
 
-const LinkCard = ({ name, href, id }) => {
+const LinkCard = ({ name, href, id, externalRoute }) => {
   return (
-    <a href={href} className={styles.card} id={id}>
-      {name}
-    </a>
+    <>
+      {!externalRoute && (
+        <Link to={href} className={styles.card} id={id}>
+          {name}
+        </Link>
+      )}
+      {externalRoute && (
+        <a href={href} className={styles.card} id={id}>
+          {name}
+        </a>
+      )}
+    </>
   );
 };
 
